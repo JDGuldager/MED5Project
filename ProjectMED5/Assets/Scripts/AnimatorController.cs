61a11012c4afb76545a869d1312ae9575caa3cc4
laurman1 <laur8250@hotmail.dk>
2024-11-07 18:44:26 +0100

    gg

--- ProjectMED5/Assets/Scripts/AnimatorController.cs
+++ ProjectMED5/Assets/Scripts/AnimatorController.cs
@@ -14,12 +14,24 @@ public class AnimatorController : MonoBehaviour
     public void Exer1()
     {
         animator.SetBool("Exer1",true);
+        animator.SetBool("Exer2", false);
     }
 
     public void Exer2()
     {
         animator.SetBool("Exer2", true);
+        animator.SetBool("Exer1", false);
     }
 
+    public void RightArm()
+    {
+        animator.SetBool("RightArm", true);
+        animator.SetBool("LeftArm", false);
+    }
 
+    public void LeftArm()
+    {
+        animator.SetBool("LeftArm", true);
+        animator.SetBool("Right", false);
+    }
 }
