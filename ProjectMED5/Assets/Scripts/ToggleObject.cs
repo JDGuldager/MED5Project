7c77bc0e71ccbb6625e0f7511c6fec37a6f20194
Jeppe Guldager <104680158+JDGuldager@users.noreply.github.com>
2024-11-12 14:26:47 +0100

    Ready for Test Ipad vs HandHeld

--- ProjectMED5/Assets/Scripts/ToggleObject.cs
+++ ProjectMED5/Assets/Scripts/ToggleObject.cs
@@ -4,6 +4,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR;
 using System.Collections.Generic;
 using System;
+using JetBrains.Annotations;
 
 public class ToggleObject : MonoBehaviour
 {
@@ -11,9 +12,10 @@ public class ToggleObject : MonoBehaviour
     public GameObject targetObject;
     public GameObject endExercise1;
     public GameObject endExercise2;
+    public GameObject text;
 
     public float timeToDisableFor = 0.5f;
-    public float endExerciseBuffer = 10;
+    public float endExerciseBuffer = 10f;
     [Range(0f, 1f)]
     public float vibrationIntensity;
     [Range(0f, 1f)]
@@ -21,7 +23,7 @@ public class ToggleObject : MonoBehaviour
 
     public ActionBasedController leftController; // Manually assigned Left XR Controller
     public ActionBasedController rightController; // Manually assigned Right XR Controller
-
+    
     // Function to be called on an OnClick event
     public void ToggleVisibility()
     {
@@ -29,9 +31,9 @@ public class ToggleObject : MonoBehaviour
         StartCoroutine(DisableTemporarily(timeToDisableFor));
         TriggerVibration(rightController);  // Example: Vibrate left controller
     }
-    public void ToggleEndExercise()
+    public void ToggleEndExercise(int exerciseNR)
     {
-        StartCoroutine(DisableEndExerciseTemporarily(endExerciseBuffer));
+        StartCoroutine(DisableEndExerciseTemporarily(endExerciseBuffer, exerciseNR));
     }
 
     // Coroutine to disable and re-enable the GameObject
@@ -46,20 +48,29 @@ public class ToggleObject : MonoBehaviour
         // Re-enable the GameObject
         targetObject.SetActive(true);
     }
+
     // Coroutine to disable and re-enable the GameObject
-    private IEnumerator DisableEndExerciseTemporarily(float time)
+    private IEnumerator DisableEndExerciseTemporarily(float time, int exerciseNR)
     {
-        // Disable the GameObject
-        endExercise1.SetActive(false);
-        endExercise2.SetActive(false);
-
-        // Wait for 0.5 seconds
+       
+        // Wait for time seconds
         yield return new WaitForSeconds(time);
-
+       
         // Re-enable the GameObject
-        endExercise1.SetActive(true);
-        endExercise2.SetActive(true);
+
+        text.SetActive(false);
+        if (exerciseNR == 1) 
+        {
+            Debug.Log("Ending Ex 1");
+            endExercise1.SetActive(true);
+        }
+        if (exerciseNR == 2)
+        {
+            Debug.Log("Ending Ex 2");
+            endExercise2.SetActive(true);
+        }
     }
+
     // Function to simulate vibration on a specific controller
     public void TriggerVibration(ActionBasedController controller)
     {
