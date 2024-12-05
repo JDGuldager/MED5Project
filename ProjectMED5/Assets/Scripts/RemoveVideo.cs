a024c040af9dfec7bd84ebb68b306e6da7de0586
Jeppe Guldager <104680158+JDGuldager@users.noreply.github.com>
2024-12-05 10:44:40 +0100

    Tuts

--- ProjectMED5/Assets/Scripts/RemoveVideo.cs
+++ ProjectMED5/Assets/Scripts/RemoveVideo.cs
@@ -7,24 +7,35 @@ using UnityEngine.InputSystem;
 public class RemoveVideo : MonoBehaviour
 {
     public ActionBasedController rightController;
-    public InputActionProperty action;
-    public float actionValue = 0;
+    public ActionBasedController leftController;
+    public InputActionProperty actionRight;
+    public InputActionProperty actionLeft;
+    public float actionValueRight = 0;
+    public float actionValueLeft = 0;
     public Vector2 stick;
 
     private void Update()
     {
         if (gameObject.tag == "Stick")
         {
-            stick = action.action.ReadValue<Vector2>();
+            stick = actionRight.action.ReadValue<Vector2>();
         }
         else
         {
-            actionValue = action.action.ReadValue<float>();
+            actionValueRight = actionRight.action.ReadValue<float>();
+            actionValueLeft = actionLeft.action.ReadValue<float>();
         }
        
         if (rightController != null)
         {
-            if (actionValue == 1 || stick != Vector2.zero)
+            if (actionValueRight == 1 || stick != Vector2.zero)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        if (leftController != null)
+        {
+            if (actionValueLeft == 1 || stick != Vector2.zero)
             {
                 gameObject.SetActive(false);
             }
