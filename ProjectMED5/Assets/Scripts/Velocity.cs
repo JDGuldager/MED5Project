6a37d0e9f46f4ddce460b0b4e782b997d49b54b9
Jeppe Guldager <104680158+JDGuldager@users.noreply.github.com>
2024-12-05 13:03:19 +0100

    Walk fixes

--- ProjectMED5/Assets/Scripts/Velocity.cs
+++ ProjectMED5/Assets/Scripts/Velocity.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Velocity : MonoBehaviour
 {
-    public float speed;
+    public ActionBasedController leftController;
+    public InputActionProperty actionLeft;
+    public float forward;
+    public float sideways;
     public Rigidbody rigidBody;
     public Animator animator;
+    public Vector2 stick;
 
     private Vector3 lastPos;
 
@@ -20,10 +26,13 @@ public class Velocity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speed = Vector3.Distance(transform.position, lastPos) / Time.deltaTime;
+        stick = actionLeft.action.ReadValue<Vector2>();
+        forward = stick.y;
+        sideways = stick.x;
 
         lastPos = transform.position;
 
-        animator.SetFloat("Forward", speed);
+        animator.SetFloat("Forward", forward);
+        animator.SetFloat("Sideways", sideways);
     }
 }
